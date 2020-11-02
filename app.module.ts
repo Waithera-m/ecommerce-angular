d8ed9f62af3af51b41173f5ac3c111ceffec6b61
Mary Waithera <njihiamary11@gmail.com>
2020-11-02 15:06:39 +0300

    initial commit

---
+++ app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EcommerceComponent } from './ecommerce/ecommerce.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EcommerceComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
